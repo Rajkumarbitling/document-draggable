0dc24bec72b171eea9a06210f2472503e9c02d73
Rajkumarbitling <rajkumarbitling@gmail.com>
2024-09-12 21:48:47 +0530

    local images added

--- src/components/DocumentCard/DocumentCard.tsx
+++ src/components/DocumentCard/DocumentCard.tsx
@@ -3,23 +3,14 @@ import Image from '../Image/Image';
 import './DocumentCard.css';
 import { DocumentCardProps } from '../../types/App.types';
 
-function getProxiedImageUrl(originalUrl: string): string {
-  return `/api/proxy-image?url=${encodeURIComponent(originalUrl)}`;
-}
+// function getProxiedImageUrl(originalUrl: string): string {
+//   return `/api/proxy-image?url=${encodeURIComponent(originalUrl)}`;
+// }
 
 const DocumentCard: React.FC<DocumentCardProps> = React.memo(({ doc, onImageClick }) => {
   const imageUrl = useMemo(() => {
-    switch (doc.type) {
-      case 'bank-draft':
-        return getProxiedImageUrl('https://loremflickr.com/640/320');
-      case 'bill-of-lading':
-        return getProxiedImageUrl('https://loremflickr.com/640/320');
-      case 'invoice':
-        return getProxiedImageUrl('https://loremflickr.com/640/320');
-      default:
-        return getProxiedImageUrl('https://loremflickr.com/640/320');
-    }
-  }, [doc.type]);
+    return `/Images/${doc.type}.jpeg`;
+  }, [doc.type]) || ''; 
 
   return (
     <div className="document-card">
